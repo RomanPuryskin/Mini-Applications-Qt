c84a279c81cf85285cd990a57f6fb61575b2b9ea
Roman <rputyskin@mail.ru>
2024-03-05 09:16:04 +0300

    Создал заголовочный файл
    В заголовочном файле объявил класс виджет и валидатор, их методы и член-данные

--- lab_work_1/widget.h
+++ lab_work_1/widget.h
@@ -2,13 +2,37 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include <QFrame>
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
+#include <QApplication>
 class Widget : public QWidget
 {
-    Q_OBJECT
-
+    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+protected:
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+public slots:
+    void begin();
+    void calc();
+};
+
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    {
+        return Acceptable; // метод всегда принимает вводимую строку
+    }
 };
 #endif // WIDGET_H
