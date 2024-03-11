e4ac1d215956d4c27ec7013e4a4cedc628fe66cd
Roman <rputyskin@mail.ru>
2024-03-11 09:13:13 +0300

    Описание widget.h
    Создано описание класса widget и класса Counter

--- lab_work_2/widget.h
+++ lab_work_2/widget.h
@@ -2,13 +2,40 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
 
 class Widget : public QWidget
 {
     Q_OBJECT
+protected:
+    QLabel *label1,*label2;
+    QPushButton *calcButton,*exitButton;
 
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 };
+
+class Counter : public QLineEdit
+{
+    Q_OBJECT
+public:
+    Counter(const QString & text , QWidget *parent = nullptr):QLineEdit(Text,parent){}
+signals:
+    void tick_signal();
+public slots:
+    void add_one()
+    {
+        QString str = text();
+        int r = str.toInt();
+        if (r!=0 && r%5 == 0)
+            emit tick_signal();
+        r++;
+        str.setNum(r);
+        setText(str);
+    }
+};
+
 #endif // WIDGET_H
