a8fcc1c08d0e76f21e13f1f1c6ce9e96a7d12a3a
Roman <rputyskin@mail.ru>
2024-03-11 15:49:10 +0300

    класс widget
    Создан и описан класс widget и кнопка закрытия окна

--- lab_work_3/widget.cpp
+++ lab_work_3/widget.cpp
@@ -1,8 +1,12 @@
 #include "widget.h"
 
-Widget::Widget(QWidget *parent)
-    : QWidget(parent)
+Widget::Widget(QWidget *parent): QWidget(parent)
 {
+    setWindowTitle("Events");
+    exitButton = new QPushButton("Завершить",this);
+    QVBoxLayout *layout1 = new QVBoxLayout(this);
+    layout1->addWidget(exitButton);
+    connect(exitButton,&QPushButton::clicked,this,&Widget::close);
 }
 
 Widget::~Widget()
--- lab_work_3/widget.h
+++ lab_work_3/widget.h
@@ -2,11 +2,15 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
 
 class Widget : public QWidget
 {
     Q_OBJECT
 
+protected:
+    QPushButton *exitButton;
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
